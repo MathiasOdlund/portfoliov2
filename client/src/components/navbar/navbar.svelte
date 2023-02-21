27827159b3ccf83345ed3bf69d55372d019ce012
Mathias Odllund <mathiasodlund@proton.me>
2023-02-21 19:42:54 +0100

    init

---
+++ client/src/components/navbar/navbar.svelte
@@ -0,0 +1,19 @@
+<script lang=ts>
+     
+</script>
+<nav class="shadow flex h-20 justify-around items-center    ">
+    <div class="title ">
+        <h1 class="text-xl">Mathias Odlund</h1>
+    </div>
+    <div class="navlinks capitalize flex">
+        <ul class="flex gap-8 decoration-none">
+            <li><a href="">Home</a></li>
+            <li><a href="">About</a></li>
+            <li><a href="">Contact</a></li>
+        </ul>
+    </div>
+</nav>
+
+<style>
+
+</style>
---
+++ client/src/routes/+layout.svelte
@@ -0,0 +1,13 @@
+<script>
+    import './layout.css'
+    import Navbar from "../components/navbar/navbar.svelte";
+</script>
+
+<main>
+    <Navbar/>
+  <slot/>
+</main>
+
+<style>
+
+</style>
